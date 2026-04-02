f46cc08d749abfa5f2ea44600d2237683ad06ba3
PULAK-SN <pulak.official22@gmail.com>
2026-04-02 10:18:22 +0530

    bug fix

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -33,7 +33,6 @@ const sendEmail = async (to, subject, text, html) => {
     });
 
     console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   } catch (error) {
     console.error("Error sending email:", error);
   }
